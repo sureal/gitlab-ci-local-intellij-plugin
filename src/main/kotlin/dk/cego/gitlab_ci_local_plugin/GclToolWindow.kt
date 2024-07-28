e6c2fd50bd67db540bfa56d4b0295ba9c5ebaca8
Matthias Neubert <matthias.neubert@deutschebahn.com>
2024-07-28 19:32:57 +0200

    Add option to disable schema validation when scanning for jobs. Some code improvements in GclToolWindow

--- src/main/kotlin/dk/cego/gitlab_ci_local_plugin/GclToolWindow.kt
+++ src/main/kotlin/dk/cego/gitlab_ci_local_plugin/GclToolWindow.kt
@@ -7,18 +7,13 @@ import com.intellij.execution.process.ProcessOutput
 import com.intellij.execution.util.ExecUtil
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.openapi.ui.popup.JBPopupFactory
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.openapi.vfs.newvfs.BulkFileListener
-import com.intellij.openapi.vfs.newvfs.events.VFileEvent
-import java.awt.Color
+import com.intellij.ui.JBColor
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.awt.event.MouseListener
@@ -30,80 +25,92 @@ import javax.swing.tree.DefaultTreeModel
 import javax.swing.tree.TreePath
 
 class GclToolWindow(private var project: Project) {
-    private var tree: JTree? = null
+    private var jobsTree: JTree? = null
     private var refreshButton: JButton? = null
     private var panel: JPanel? = null
     private var checkBox: JCheckBox? = null
+    private var schemaValidationCheckBox: JCheckBox? = null
 
     init {
-        this.tree?.model = DefaultTreeModel(DefaultMutableTreeNode(this.project.name))
+        this.jobsTree?.model = DefaultTreeModel(DefaultMutableTreeNode(this.project.name))
+        setupRefreshButton()
+        refresh()
+    }
+
+    private fun setupRefreshButton() {
         refreshButton!!.icon = AllIcons.Actions.Refresh
         refreshButton!!.text = "Refresh"
         refreshButton!!.addActionListener { refresh() }
-        refresh()
     }
 
     private fun refresh() {
         val task: Task.Backgroundable = object : Task.Backgroundable(project, "Fetching Gitlab-CI jobs...", true) {
             override fun run(indicator: ProgressIndicator) {
                 refreshButton?.text = "..."
-                var errorMessage = "";
-                var output: ProcessOutput? = null;
                 try {
-                    output = gclList()
-                } catch (e: ExecutionException) {
-                    errorMessage = e.message!!
-                }
+                    val output = runGclListCommand(schemaValidationCheckBox!!.isSelected)
+                    if (output.exitCode != 0) {
+                        val errorMessage = output.stderr.ifEmpty { output.stdout }
+                        showErrorMessage(errorMessage)
+                    } else {
+                        // parse output
+                        val gclJobs = GclJobFactory().parse(output.stdout)
 
-                if (output != null) {
-                    errorMessage = output.stderr.ifEmpty { output.stdout }
-                }
-                if (output == null || output.exitCode != 0) {
-                    // show IDE balloon
-                    ApplicationManager.getApplication().invokeLater {
-                        JBPopupFactory.getInstance()
-                            .createHtmlTextBalloonBuilder(
-                                "<html>Error:<br>${errorMessage}</html>",
-                                null,
-                                Color.BLACK,
-                                Color.RED,
-                                null
-                            )
-                            .createBalloon()
-                            .show(
-                                JBPopupFactory.getInstance()
-                                    .guessBestPopupLocation(refreshButton!!),
-                                Balloon.Position.atRight
-                            )
+                        // update UI
+                        ApplicationManager.getApplication().invokeLater {
+                            showGclJobsInJobTree(gclJobs)
+                        }
                     }
-                    refreshButton?.text = "Refresh"
-                    return
+                } catch (e: ExecutionException) {
+                    e.message?.let { showErrorMessage(it) }
                 }
+                refreshButton?.text = "Refresh"
+            }
 
-                // parse output
-                val gclJobs = GclJobFactory().parse(output.stdout)
-
-                // update UI
-                ApplicationManager.getApplication().invokeLater {
-                    showGclJobs(gclJobs)
-                }
+            override fun onCancel() {
+                super.onCancel()
                 refreshButton?.text = "Refresh"
             }
         }
         ProgressManager.getInstance().run(task)
     }
 
-    private fun gclList(): ProcessOutput {
-        val runConfiguration =
-            GeneralCommandLine(WslUtils.rewriteToWslExec(project.basePath!!, listOf("gitlab-ci-local", "--list-json")))
-                .withRedirectErrorStream(true)
-        runConfiguration.workDirectory = File(project.basePath!!)
-        return ExecUtil.execAndGetOutput(runConfiguration)
+    private fun showErrorMessage(errorMessage: String) {
+        // show IDE balloon
+        ApplicationManager.getApplication().invokeLater {
+            JBPopupFactory.getInstance()
+                .createHtmlTextBalloonBuilder(
+                    "<html>Error:<br>${errorMessage}</html>",
+                    null,
+                    JBColor.BLACK,
+                    JBColor.RED,
+                    null
+                )
+                .createBalloon()
+                .show(
+                    JBPopupFactory.getInstance()
+                        .guessBestPopupLocation(refreshButton!!),
+                    Balloon.Position.atRight
+                )
+        }
     }
 
-    fun showGclJobs(jobs: List<GclJob>) {
+    private fun runGclListCommand(validateSchema: Boolean): ProcessOutput {
+        val commandWithArgs = listOf(
+            "gitlab-ci-local",
+            "--list-json",
+            "--json-schema-validation=${validateSchema}"
+        )
+        val cliCommand = GeneralCommandLine(
+            WslUtils.rewriteToWslExec(project.basePath!!, commandWithArgs)
+        ).withRedirectErrorStream(true)
+        cliCommand.workDirectory = File(project.basePath!!)
+        return ExecUtil.execAndGetOutput(cliCommand)
+    }
+
+    fun showGclJobsInJobTree(jobs: List<GclJob>) {
         // group jobs by stage
-        val stages = ArrayList<GclStage>()
+        val stages = mutableListOf<GclStage>()
         for (job in jobs) {
             var stage = stages.find { it.stage == job.stage }
             if (stage == null) {
@@ -114,7 +121,7 @@ class GclToolWindow(private var project: Project) {
             stage.jobs.add(job)
         }
 
-        // create tree
+        // create jobsTree
         val root = DefaultMutableTreeNode(project.name)
 
         for (stage in stages) {
@@ -130,40 +137,39 @@ class GclToolWindow(private var project: Project) {
         renderer.openIcon = AllIcons.Nodes.Folder
         renderer.closedIcon = AllIcons.Nodes.Folder
         // on
-        tree?.cellRenderer = renderer
-        tree?.model = DefaultTreeModel(root)
+        jobsTree?.cellRenderer = renderer
+        jobsTree?.model = DefaultTreeModel(root)
 
-        for (i in 0 until tree!!.visibleRowCount) {
-            tree!!.expandPath(tree!!.getPathForRow(i))
+        for (i in 0 until jobsTree!!.visibleRowCount) {
+            jobsTree!!.expandPath(jobsTree!!.getPathForRow(i))
         }
 
-        // add mouse listener
-        addMouseListener()
-        tree?.fireTreeExpanded(TreePath(root))
+        addMouseListener(jobsTree!!)
+        jobsTree?.fireTreeExpanded(TreePath(root))
     }
 
-
-    private fun addMouseListener() {
+    private fun addMouseListener(tree: JTree) {
         val ml: MouseListener = object : MouseAdapter() {
             override fun mousePressed(e: MouseEvent) {
-                val selRow = tree!!.getRowForLocation(e.x, e.y)
-                val selPath: TreePath? = tree!!.getPathForLocation(e.x, e.y)
-                if (selPath != null && selPath.pathCount > 2 && selRow != -1 && e.clickCount == 2) {
-                    val script =
-                        selPath.path[2].toString() + " " + (if (checkBox!!.isSelected) "--needs" else "--no-needs")
-                    val runner: RunnerAndConfigurationSettings = RunManager.getInstance(project)
-                        .createConfiguration(script, GclRunConfigurationType::class.java)
-                    RunManager.getInstance(project).addConfiguration(runner)
+                val selectedRow = tree.getRowForLocation(e.x, e.y)
+                val selectedPath: TreePath? = tree.getPathForLocation(e.x, e.y)
+                if (selectedPath != null && selectedPath.pathCount > 2 && selectedRow != -1 && e.clickCount == 2) {
+
+                    val needsArgument = if (checkBox!!.isSelected) "--needs" else "--no-needs"
+                    val schemaValidationArgument = "--schema-validation=${schemaValidationCheckBox!!.isSelected}"
+                    val script = "${selectedPath.path[2]} $needsArgument $schemaValidationArgument"
+                    val runManager = RunManager.getInstance(project)
+                    val configuration = runManager.createConfiguration(script, GclRunConfigurationType::class.java)
+                    runManager.addConfiguration(configuration)
                     // set the configuration as the selected configuration
-                    RunManager.getInstance(project).selectedConfiguration = runner
+                    runManager.selectedConfiguration = configuration
                     val executor: Executor = DefaultRunExecutor.getRunExecutorInstance()
-                    ProgramRunnerUtil.executeConfiguration(runner, executor)
+                    ProgramRunnerUtil.executeConfiguration(configuration, executor)
                 }
             }
         }
-        tree!!.addMouseListener(ml)
+        tree.addMouseListener(ml)
     }
-
 
     val content: JComponent?
         get() = panel
